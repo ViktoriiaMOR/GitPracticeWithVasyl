f8f99da67d683aaabc7ad9329bbf908415682c8e
ViktoriiaMOR <morgonovavika@mail.ru>
2021-01-13 00:34:59 -0500

    commit number 1

--- src/test/java/tests/testing_pull_requests_2.java
+++ src/test/java/tests/testing_pull_requests_2.java
@@ -3,7 +3,7 @@ package tests;
 public class testing_pull_requests_2 {
     public static void main(String[] args) {
 
-        System.out.println("Let's test this baby69");
+        System.out.println("commit number 1");
 
     }
 }
